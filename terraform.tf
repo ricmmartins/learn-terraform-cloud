27ec70945a3726cf7a392402d00c161ebab83b93
Ricardo Martins <44813563+ricmmartins@users.noreply.github.com>
2024-01-02 13:18:27 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "rmmartins"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
